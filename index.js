8e2ffdef4b485dc9f85e6e63982d9733b3309e3a
Diego <dardila90@gmail.com>
2020-07-29 18:28:18 -0500

    Adding comment

--- index.js
+++ index.js
@@ -29,3 +29,5 @@ arrOfDucks.suscribeFunction(huntingNormalDucks)
 arrOfDucks.suscribeFunction(huntingWildDucks)
 arrOfDucks.suscribeFunction(huntingCloudDucks)
 arrOfDucks.fireFunctions()
+
+//Comentario desde diego_branch
